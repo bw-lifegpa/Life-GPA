39476506c5ccd756bebb0216c500087e4ac5ec87
ch04937 <97hernandez.c@gmail.com>
2019-08-26 12:57:03 -0700

    added libraries and created login page with formik

---
+++ life-gpa/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import FormikLogin from './components/Login';
+import Dashboard from './components/Dashboard';
+import PrivateRoute from './components/PrivateRoute';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <div className='login-form-page'>
+          <Route extact path ='/' component={FormikLogin} />
+          <PrivateRoute extact path='/dashboard' component={Dashboard} />
+        </div>
+      </div>
+    </Router>
+
+
+  );
+}
+
+export default App;
---
+++ life-gpa/src/components/Login.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import axios from 'axios';
+import { Form, Field, withFormik } from 'formik';
+import { withRouter } from 'react-router-dom';
+import * as yup from 'yup';
+
+import './login.css'
+
+function Login({ errors, touched }) {
+    return(
+        <div className='login-page'>
+            <h2 className='login-header'>Welcome Back!</h2>
+            <Form className='login-form-field' >
+                {touched.username && errors.username && <p>{errors.username}</p>}
+                Username or Email: 
+                <Field type= 'username' name='username' placeholder='Username' />
+                {touched.password && errors.password && <p>{errors.password}</p>}
+                Password: 
+                <Field type= 'password' name='password' placeholder='password' />
+                <button className='button' type='submit'>Get Ro Tracking</button>
+
+            </Form>
+
+        </div>
+    )
+}
+
+const FormikLogin = withRouter(withFormik({
+    mapPropsToValues({ username, password }){
+        return{
+            username: username || '',
+            password: password || ''
+        };
+    },
+    validationSchema: yup.object().shape({
+        username: yup.string().required('username is required'),
+        password: yup.string().required('password is required'),
+        
+    }),
+    handleSubmit(users, {props}){
+        axios.post('')
+    }
+})(Login));
+
+export default FormikLogin
